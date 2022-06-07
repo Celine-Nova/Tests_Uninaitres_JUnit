95da27b0b2b6ae637181fd7963252cf2f45fa0bf
Celine-Nova <cocotte243@gmail.com>
2022-06-07 13:41:16 +0200

    plusieurs tests sur la methode add de la classe Calculator

--- src/test/java/com/poe/demotesunitaire/caltulator/CalculatorTest.java
+++ src/test/java/com/poe/demotesunitaire/caltulator/CalculatorTest.java
@@ -15,16 +15,51 @@ import org.junit.jupiter.api.Test;
  */
 public class CalculatorTest {
     
+     @Test
+    public void testAdd3plus2retourne5() {
+        Calculator calculator = new Calculator();
+        
+        int result = calculator.add(3, 2);
+        
+        assertEquals(5, result);
+    }
+    
     @Test
-    public void testAdd(){
-    Calculator calculator = new Calculator();
-    int result = calculator.add(1,2);
-    assertEquals(3, result);
-}
+    public void testAdd2plus1retourne3() {
+        Calculator calculator = new Calculator();
+        
+        int result = calculator.add(2, 1);
+        
+        assertEquals(3, result);
+    }
+    
+    
+    @Test
+    public void testAdd0plus0retourne0() {
+        Calculator calculator = new Calculator();
+        
+        int result = calculator.add(0, 0);
+        
+        assertEquals(0, result);
+    }
+    
+    @Test
+    public void testAddMoinsCinqAndMoins4retourneMoinsNeuf() {
+        Calculator calculator = new Calculator();
+        
+        int result = calculator.add(-5, -4);
+        
+        assertEquals(-9, result);
+    }
+    
+    @Test
+    public void testGrandNombres() {
+         Calculator calculator = new Calculator();
+        
+        int result = calculator.add(5000, 10000);
+        
+        assertTrue(result == 15000);
+    }
 
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
+
 }
