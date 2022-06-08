6197a07a532169c8a2262d8653fde7f3a4249e6b
Celine-Nova <cocotte243@gmail.com>
2022-06-08 12:02:23 +0200

    découverte @Parametrized avec @ValueSource et @CsvSource

--- src/test/java/com/poe/demotestsunitaires/caltulator/CalculatorTest.java
+++ src/test/java/com/poe/demotestsunitaires/caltulator/CalculatorTest.java
@@ -6,8 +6,12 @@ package com.poe.demotestsunitaires.caltulator;
  */
 
 import com.poe.demotestdunitaired.calculator.Calculator;
+import java.util.ArrayList;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 /**
  *
@@ -16,7 +20,7 @@ import org.junit.jupiter.api.Test;
 public class CalculatorTest {
     
      @Test
-    public void testAdd3plus2retourne5() {
+    public void testAdd3Add2return5() {
         Calculator calculator = new Calculator();
         
         int result = calculator.add(3, 2);
@@ -25,7 +29,7 @@ public class CalculatorTest {
     }
     
     @Test
-    public void testAdd2plus1retourne3() {
+    public void testAdd2Add1return3() {
         Calculator calculator = new Calculator();
         
         int result = calculator.add(2, 1);
@@ -35,7 +39,7 @@ public class CalculatorTest {
     
     
     @Test
-    public void testAdd0plus0retourne0() {
+    public void testAdd0Add0return0() {
         Calculator calculator = new Calculator();
         
         int result = calculator.add(0, 0);
@@ -44,7 +48,7 @@ public class CalculatorTest {
     }
     
     @Test
-    public void testAddMoinsCinqAndMoins4retourneMoinsNeuf() {
+    public void testAddLess5AndLess4returnLes9() {
         Calculator calculator = new Calculator();
         
         int result = calculator.add(-5, -4);
@@ -53,7 +57,7 @@ public class CalculatorTest {
     }
     
     @Test
-    public void testGrandNombres() {
+    public void testBigNumber() {
          Calculator calculator = new Calculator();
         
         int result = calculator.add(5000, 10000);
@@ -98,10 +102,47 @@ public class CalculatorTest {
         assertEquals(10/3, result);
     }
     @Test
-    public void tesDivideWithDivideAsZero(){
+    public void testDivideWithDivideAsZero(){
         Calculator calculator = new Calculator();
         assertThrows(ArithmeticException.class, () ->{
             calculator.dividende(10,0);
         });
+    }
+   @Test
+   public void TestImpairNumber(){
+       Calculator calculator = new Calculator();
+       ArrayList<Integer>result = calculator.getImpairNumbers(5);
+        assertEquals(3, result.size());
+   }
+   //EXEMPLES avec des PARAMS
+   //  @ParameterizedTest stipule qu'on va tester avec des paramètres
+    @ParameterizedTest
+    //@ValueSource je donne les valeurs pour les paramètres
+    // Parmet  d'executer un seul test plusieurs fois
+    @ValueSource(ints = {1, 2, 3, 4})
+    void testParameterWithValueSource(int valeur) {
+        assertEquals(valeur + valeur, valeur * 2);
+    }
+
+    @ParameterizedTest
+    // @CsvSource A contrario de @ValueSource ici nous pouvons donner plusieurs valeurs pour un seul paramètre
+    @CsvSource({"1, 1", "1, 2", "2, 3"})
+    void testAdd(int a, int b) {
+        int attendu = a + b;
+        assertEquals(attendu, a + b);
+    }
+    
+    @ParameterizedTest
+    @CsvSource({"1, 1, 2", "1, 2, 3", "2, 3, 5"})
+    void testAddWithResult(int a, int b, int result) {
+        assertEquals(result, a + b);
+    }
+        
+    @ParameterizedTest
+    @CsvSource({"1, 1, 1", "1, 2, 2", "5, 3, 5", "-1, -4, -1"})
+    void testMaxWithDatas(int a, int b, int result) {
+        Calculator calculator = new Calculator();
+        
+        assertEquals(result, calculator.maxValue(a, b));
     }
 }
