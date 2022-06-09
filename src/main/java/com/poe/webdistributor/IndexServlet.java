9288ba93c976b7a226e9f4535cbdca1a30c3ef4f
Celine-Nova <cocotte243@gmail.com>
2022-06-10 00:39:53 +0200

    structure webdistributor ajout servlet + jsp

--- src/main/java/com/poe/webdistributor/IndexServlet.java
+++ src/main/java/com/poe/webdistributor/IndexServlet.java
@@ -7,11 +7,12 @@ package com.poe.webdistributor;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
+@WebServlet("/")
 public class IndexServlet extends HttpServlet {
 
     /**
@@ -52,7 +53,7 @@ public class IndexServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        request.getServletContext().getRequestDispatcher("/WEB-INF/index.jsp").forward(request, response);
     }
 
     /**
--- src/main/webapp/WEB-INF/index.jsp
+++ src/main/webapp/WEB-INF/index.jsp
@@ -1,10 +1,13 @@
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Start Page</title>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
     </head>
     <body>
         <h1>Hello World!</h1>
+        <p>Je suis la page JSP</p>
     </body>
 </html>
